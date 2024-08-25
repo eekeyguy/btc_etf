3de2df12bb7369bc7d0d168b62fa386d8c9cf517
eekeyguy <128739145+eekeyguy@users.noreply.github.com>
2024-08-25 16:13:48 +0530

    Update btc_flows_coinglass.py

--- btc_flows_coinglass.py
+++ btc_flows_coinglass.py
@@ -63,6 +63,7 @@ def get_table_data(driver):
     for row in rows:
         cells = row.find_elements(By.TAG_NAME, 'td')
         data = [cell.text for cell in cells]
+        print(data)
         data_rows.append(data)
 
     return headers, data_rows
